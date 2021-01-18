53c24f0927033a5172fcb30a982ebbe07c0330f3
EV-55 <77371444+EV-55@users.noreply.github.com>
2021-01-18 11:19:54 +0700

    Calculator_Length_Circle_L_and_Area_Circle_S.kt

--- src/main.kt
+++ src/main.kt
@@ -1,16 +1,16 @@
 fun main()
 {
   println("Вас приветствует программа Калькулятор длины окружности L и площади круга S.")
-  println("Программа создана Bazilevs Technology.")
-    val pi = 3.14
+  println("Программа создана KvantoriumTeam.")
+  println("Число π всегда равно 3,14(примерно)")
+  val pi = 3.14
+  println (pi)
   print("Введите радиус окружности.")
   println(" После введения нажмите Enter.")
     val R = readLine()!!.toInt()
     val R2 = R*R
     val L = 2 * pi * R
     val S = pi * R2
-  println("L=")
-  println(L)
-  println("S=")
-  println(S)
+  println("L = $L")
+  println("S = $S")
 }
